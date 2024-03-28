1e45b2e5f1f1f6a738a0032e168ac225f0ee3131
Emanuel Mineda Carneiro <mineda@gmail.com>
2024-03-28 00:22:31 +0000

    Axios

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://8080-mineda-springboot3lab4-11o9c17xhco.ws-us110.gitpod.io/'
+
 const app = createApp(App)
 
 app.use(createPinia())
--- src/views/UsuarioView.vue
+++ src/views/UsuarioView.vue
@@ -5,6 +5,10 @@
         <p><input type="text" v-model="nome"/></p>
         <p v-if="nome.length > 10">Nome muito longo!</p>
         <div v-else>Nome ok!</div>
+        <p><input type="password" v-model="senha"/></p>
+        <button @click="buscarUsuarios">Atualizar</button>
+        <button @click="cadastroUsuario">Cadastrar</button>
+        <p v-if="erro">{{ erro }}</p>
         <TabelaUsuario :usuarios="usuarios"/> 
 
     </div>
@@ -13,8 +17,12 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import TabelaUsuario from '@/components/TabelaUsuario.vue';
+import axios from 'axios';
+import { onMounted } from 'vue';
 
 const nome = ref("Teste");
+const senha = ref("123456");
+const erro = ref("");
 
 const usuarios = ref(
     [ 
@@ -22,10 +30,34 @@ const usuarios = ref(
         {id: 2, nome: "user"} 
     ]);
 
+async function buscarUsuarios() {
+    usuarios.value = (await axios.get('usuario')).data;
+}
+
+async function cadastroUsuario() {
+    try {
+        usuarios.value.push((await axios.post('usuario', 
+            { 
+                nome: nome.value, 
+                senha: senha.value 
+            }
+        )).data);
+        erro.value = "";
+    }
+    catch(exception) {
+        erro.value = (exception as Error).message;
+    }
+
+}
+
 function mudouNome(event: Event) {
     nome.value = (event.target as HTMLInputElement).value;
 
 }
+
+onMounted(() => {
+    buscarUsuarios();
+});
 
 </script>
 
